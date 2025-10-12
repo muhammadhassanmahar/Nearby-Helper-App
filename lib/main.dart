9f70398a21d5e9172c395595656ba07c9c8823d6
Muhammad Hassan <immaharhasaan@gmail.com>
2025-10-12 16:31:55 +0500

    Update: Nearby Helper App improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nearby_helper_app/screens/requests_list_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/add_request_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      title: 'Nearby Helper',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+
+      // 🗺️ Define app routes
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/add-request': (context) => const AddRequestScreen(),
+        '/requests-list': (context) => const RequestsListScreen(),
+      },
     );
   }
 }
